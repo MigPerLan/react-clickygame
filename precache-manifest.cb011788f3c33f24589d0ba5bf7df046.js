00a7c75a7ed121b89d7fcbb326d4b5d32af53a79
Mikeyhey <mikeyhey@hotmail.com>
2019-03-31 21:41:54 -0700

    Updates

---
+++ precache-manifest.cb011788f3c33f24589d0ba5bf7df046.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "962b99b7ef932cd7ff99",
+    "url": "/react-clickygame/static/js/runtime~main.a2679394.js"
+  },
+  {
+    "revision": "243fd0a58e1ec302d6d8",
+    "url": "/react-clickygame/static/js/main.a8b24872.chunk.js"
+  },
+  {
+    "revision": "baa2049f8ea9483c525a",
+    "url": "/react-clickygame/static/js/2.9020a117.chunk.js"
+  },
+  {
+    "revision": "243fd0a58e1ec302d6d8",
+    "url": "/react-clickygame/static/css/main.c4106d86.chunk.css"
+  },
+  {
+    "revision": "baa2049f8ea9483c525a",
+    "url": "/react-clickygame/static/css/2.f48f1d2c.chunk.css"
+  },
+  {
+    "revision": "b75576679400192e38540b0ec2cc2844",
+    "url": "/react-clickygame/index.html"
+  }
+];
