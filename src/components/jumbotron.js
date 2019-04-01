5f197f3314e82114ed6eba2d3f9f833655e46a03
Mikeyhey <mikeyhey@hotmail.com>
2019-03-31 21:17:15 -0700

    bug fixes

--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -8,7 +8,7 @@ class Jumbotron extends React.Component {
             <div>
                 <div className="jumbotron jumbotron-fluid">
                     <div className="container">
-                       <h1 className="display-4">Shaq Soda Clicky Game</h1>
+                       <h1 className="display-4">Shaq Soda Clicky Game!</h1>
                        <p>Click on some refreshing Shaq Soda to start the game!</p>
                         <h2 className="display-4">{this.props.score()}</h2>
                         
